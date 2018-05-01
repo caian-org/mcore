fd3c116d289e0edf05771cdf4d49584a4103b76a
Caian R. Ertl <caianrais@protonmail.com>
2018-04-30 23:07:06 -0300

    minor: pequenas correções e melhoramentos

--- src/tests/test_models.py
+++ src/tests/test_models.py
@@ -1,27 +1,36 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+--- TODO: DOCUMENTATION ---
+"""
+
+# Standard libraries
 import sys
 import unittest
 from os import path
 from datetime import datetime
 
-
+# Parent directory "injection"
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
-
+# Modules
 from mapi import db
 from mapi.models import (Address,
                          Worker,
                          WorkerAddressAssoc,
                          Vehicle)
 
+# 3rd-party libraries
 from faker import Faker
 br_fake = Faker('pt_BR')
 us_fake = Faker('en_US')
 
 
 class TestModels(unittest.TestCase):
+    """
+    --- TODO: DOCUMENTATION ---
+    """
 
     def test_a_address(self):
         """
@@ -120,7 +129,7 @@ class TestModels(unittest.TestCase):
         caian = Worker.query.get(1)
         diogo = Worker.query.get(2)
 
-        self.assertEqual(len(caian.vehicles), 5) and self.assertEqual(len(diogo.vehicles), 5)
+        self.assertEqual(len(caian.vehicles), len(diogo.vehicles))
 
     def test_g_worker_has_addresses(self):
         """
@@ -142,6 +151,9 @@ class TestModels(unittest.TestCase):
 
         worker_addresses = db.session.query(WorkerAddressAssoc).join(Worker).filter(Worker.cpf == '45354686806').all()
         self.assertEqual(len(worker_addresses), 2)
+
+    def test_h_company_has_addresses(self):
+        pass
 
 
 if __name__ == '__main__':
