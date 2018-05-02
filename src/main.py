17f588294776d4ce8bbe802249f87794db2da622
Caian R. Ertl <caianrais@protonmail.com>
2018-05-02 03:05:38 -0300

    refact: reestrutura para um esquema mais explícito

--- src/main.py
+++ src/main.py
@@ -1,4 +1,20 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+--- TODO: DOCUMENTATION ---
+"""
+
 from mapi import app
+from mapi import Config
+
+from mapi.routes import Router
+
+
+def main():
+    Router.act()
+    app.run(debug=Config.DEBUG)
+
+
+if __name__ == '__main__':
+    main()
