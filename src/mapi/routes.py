9666865f8879a5a5d4734bc49060968ba934fae9
Caian R. Ertl <caianrais@protonmail.com>
2018-05-02 03:03:49 -0300

    feat: implementa estrutura base das rotas

--- src/mapi/routes.py
+++ src/mapi/routes.py
@@ -4,9 +4,39 @@
 --- TODO: DOCUMENTATION ---
 """
 
-from mapi import app
+from mapi import (rapi, Resource)
+from mapi import __version__
+from mapi import __parent_resource__
+
+from mapi.models import Worker
 
 
-@app.route('/')
-def index():
-    return 'hello world!'
+class WorkerAuthentication(Resource):
+    """
+    --- TODO: DOCUMENTATION ---
+    """
+
+    def post(self):
+        """
+        --- TODO: DOCUMENTATION ---
+        """
+        pass
+
+class Router:
+    """
+    --- TODO: DOCUMENTATION ---
+    """
+
+    @staticmethod
+    def format(route):
+        """
+        --- TODO: DOCUMENTATION ---
+        """
+        return '/{0}/{1}/{2}'.format(__parent_resource__, __version__, route)
+
+    @staticmethod
+    def act():
+        """
+        --- TODO: DOCUMENTATION ---
+        """
+        rapi.add_resource(WorkerAuthentication, Router.format('workers/auth'))
