5d42f1a02436a36cdfdc0e80cd60cc8595a3c29e
Caian R. Ertl <caianrais@protonmail.com>
2018-05-06 03:24:46 -0300

    feat: implementa estrutura básica de schema para modelo

---
+++ src/mapi/schemas.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+--- TODO: DOCUMENTATION ---
+"""
+
+from mapi.orm import Schema
+from mapi.models import Worker
+
+
+class WorkerSchema(Schema):
+    """
+    --- TODO: DOCUMENTATION ---
+    """
+    class Meta:
+        model = Worker
