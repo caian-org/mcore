10e78ea275d427a06f10222159c6897d43ea09a3
Caian R. Ertl <caianrais@protonmail.com>
2018-05-06 04:37:20 -0300

    refact: inclui definição de porta do socket do Flask

--- src/config.py
+++ src/config.py
@@ -11,6 +11,7 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
+    PORT = 80
     DEBUG = False
     TESTING = False
     SQLALCHEMY_DATABASE_URI = ''
@@ -24,5 +25,6 @@ class Production(Config):
 
 
 class Development(Config):
+    PORT = 8080
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
