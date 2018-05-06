198b6e34c1fcc8ec15911fd4bd2d11997ffe8f51
Caian R. Ertl <caianrais@protonmail.com>
2018-05-05 21:38:08 -0300

    feat: inclui tratamento de erro para as bibliotecas 3rd-party

--- src/mapi/resources/__init__.py
+++ src/mapi/resources/__init__.py
@@ -4,21 +4,23 @@
 --- TODO: DOCUMENTATION ---
 """
 
-# Standard libraries
 import sys
 from os import path
-
-# 3rd-party libraries
-from flask import request
-from flask_restful import Resource
 
 
 # Parent directory "injection"
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 
-# Models module
+from mapi.utils import Exit
 from mapi.models import *
 
-# Resources utilitaries
+try:
+    from flask import request
+    from flask_restful import Resource
+
+except ImportError as error:
+    Exit.with_fail('Impossible to import 3rd-party libraries\n'
+                   'Latest traceback: {0}' . format(error.args[0]))
+
 from mapi.resources.responder import Response
