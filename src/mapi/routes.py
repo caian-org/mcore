fdcf322a750abbe66934d800e8043a4f8025e732
Caian R. Ertl <caianrais@protonmail.com>
2018-05-06 07:21:42 -0300

    fix: corrige referência de classe na criação de recursos/rotas

--- src/mapi/routes.py
+++ src/mapi/routes.py
@@ -8,8 +8,8 @@
 from mapi import rapi
 from mapi import Formatter
 
-from mapi.resources.address import (Address, AddressInclusion)
-from mapi.resources.worker import (Worker, WorkerAuthentication)
+from mapi.resources.address import (AddressRecord, AddressNew)
+from mapi.resources.worker import (WorkerRecord, WorkerAuth)
 
 
 class Router:
@@ -22,8 +22,8 @@ class Router:
         """
         --- TODO: DOCUMENTATION ---
         """
-        rapi.add_resource(WorkerAuthentication, Formatter.gen_route('workers/auth'))
-        rapi.add_resource(Worker, Formatter.gen_route('workers/<int:worker_id>'))
+        rapi.add_resource(WorkerAuth, Formatter.gen_route('workers/auth'))
+        rapi.add_resource(WorkerRecord, Formatter.gen_route('workers/<int:worker_id>'))
 
-        rapi.add_resource(AddressInclusion, Formatter.gen_route('addresses'))
-        rapi.add_resource(Address, Formatter.gen_route('addresses/<int:address_uid>'))
+        rapi.add_resource(AddressNew, Formatter.gen_route('addresses'))
+        rapi.add_resource(AddressRecord, Formatter.gen_route('addresses/<int:address_uid>'))
