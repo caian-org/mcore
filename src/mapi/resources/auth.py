d9578850950d55b9cd3a43c6bc110569d6148aa2
Caian R. Ertl <caianrais@protonmail.com>
2018-05-06 07:15:03 -0300

    feat: implementa autenticador de estrutura de payload e token

---
+++ src/mapi/resources/auth.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""
+--- TODO: DOCUMENTATION ---
+"""
+
+from . import Person
+
+
+class Authenticator:
+    @staticmethod
+    def check_struct(payload):
+        elements = [ payload.get('auth'), payload.get('data') ]
+        print(repr(elements))
+
+        for element in elements:
+            if not isinstance(element, dict):
+                return False
+
+        return True
+
+    @staticmethod
+    def check_payload(params):
+        for param in params:
+            if not param:
+                return False
+
+        return True
+
+    @staticmethod
+    def verify_token(entity, token):
+        if entity not in ['worker', 'company']:
+            return False
+
+        return Person.verify_token(token)
