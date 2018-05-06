4557615c08e9a5c107f9a8810ef8ef52eb9a3cfa
Caian R. Ertl <caianrais@protonmail.com>
2018-05-06 01:20:21 -0300

    refact: inclui bibliotecas de terceiros no inicializador do módulo

--- src/mapi/__init__.py
+++ src/mapi/__init__.py
@@ -16,19 +16,38 @@ from mapi.utils import Exit
 
 # 3rd-party libraries
 try:
+    from faker import Faker
+
     # Flask itself
     from flask import Flask
 
     # REST API abstraction layer
-    from flask_restful import Api
+    from flask_restful import Api as Restful
 
     # ORM-related
     from flask_migrate import Migrate
     from flask_sqlalchemy import SQLAlchemy
 
+    # Security modules (login-related)
+    from werkzeug.security import (generate_password_hash as gen_phash,
+                                   check_password_hash as check_phash)
+
+    # Security modules (token-related)
+    from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer,
+                              BadSignature, SignatureExpired)
+
+    # Mail service API
+    import pycep_correios as cep_api
+    from pycep_correios.excecoes import (Timeout, FalhaNaConexao,
+                                         MultiploRedirecionamento)
+
 except ImportError as error:
     Exit.with_fail('Impossible to import 3rd-party libraries\n'
                    'Latest traceback: {0}' . format(error.args[0]))
+
+# Very long (and secret) key
+from mapi.secret import SECRET_KEY
+
 
 # What kind of configuration should be used?
 if os.environ.get('TEST_ENVIRON'):
@@ -38,10 +57,12 @@ else:
     from config import Production as Config
 
 
+Config.SECRET_KEY = SECRET_KEY
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
-rapi = Api(app)
+rapi = Restful(app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
