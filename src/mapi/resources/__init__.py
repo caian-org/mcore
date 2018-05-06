1f7622d5555418639aa28c971fbd45736bf6a3e8
Caian R. Ertl <caianrais@protonmail.com>
2018-05-06 01:46:06 -0300

    refact: importa bibliotecas do nível do módulo base

--- src/mapi/resources/__init__.py
+++ src/mapi/resources/__init__.py
@@ -12,15 +12,9 @@ from os import path
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 
+from mapi import (request, Resource, BadRequest)
+
 from mapi.utils import Exit
 from mapi.models import *
 
-try:
-    from flask import request
-    from flask_restful import Resource
-
-except ImportError as error:
-    Exit.with_fail('Impossible to import 3rd-party libraries\n'
-                   'Latest traceback: {0}' . format(error.args[0]))
-
-from mapi.resources.responder import Response
+from mapi.resources.responder import response
