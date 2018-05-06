7d0eaaaa2c0553beaead7830669ac61cc513aac6
Caian R. Ertl <caianrais@protonmail.com>
2018-05-06 01:47:46 -0300

    feat: implementa fluxo de autenticação do trabalhador

--- src/mapi/resources/worker.py
+++ src/mapi/resources/worker.py
@@ -5,7 +5,9 @@
 """
 
 from . import Worker
-from . import (Response, Resource, request)
+from . import (Resource, request, response)
+
+from . import BadRequest
 
 
 class WorkerAuthentication(Resource):
@@ -17,17 +19,23 @@ class WorkerAuthentication(Resource):
         """
         --- TODO: DOCUMENTATION ---
         """
-        email = request.form['email']
-        worker = Worker.query.filter_by(email=email).all()
+        try:
+            email = request.form['email']
+            passw = request.form['password']
+        except BadRequest as error:
+            return response.FAIL(400, error.description)
 
+        worker = Worker.query.filter_by(email=email).all()
         if not worker:
-            error = { 'description': 'User not found' }
-            return Response.FAIL(404, error)
+            return response.incorrect_email_or_password
+
+        worker = worker[0]
+        valid_passw = worker.verify_password(passw)
+        if not valid_passw:
+            return response.incorrect_email_or_password
+
+        token = worker.generate_token()
 
         data = {}
-        data['worker'] = { 'name': worker[0].name }
-        return Response.SUCCESS(200, data)
-
-
-class WorkerRegistry(Resource):
-    pass
+        data['token'] = token.decode('ascii')
+        return response.SUCCESS(200, data)
