d7b1443a92f383c2d72a616a065504e5a3d7306e
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-05-07 20:18:45 -0300

    style: inclui comentários

--- src/mapi/routes.py
+++ src/mapi/routes.py
@@ -35,24 +35,31 @@ class Router:
         --- TODO: DOCUMENTATION ---
         """
 
+        # Workers
         Router.include(WorkerNew, 'workers')
         Router.include(WorkerAuth, 'workers/auth')
         Router.include(WorkerRecord, 'workers/<int:worker_id>')
 
+        # Addresses
         Router.include(AddressNew, 'addresses')
         Router.include(AddressRecord, 'addresses/<int:address_id>')
 
+        # Companies
         Router.include(CompanyNew, 'companies')
         Router.include(CompanyRecord, 'companies/<int:company_id>')
 
+        # Items
         Router.include(ItemNew, 'items')
         Router.include(ItemRecord, 'items/<int:item_id>')
 
+        # Offers
         Router.include(OfferNew, 'offers')
         Router.include(OfferRecord, 'offers/<int:offers_id>')
 
+        # Proposals
         Router.include(ProposalNew, 'proposals')
         Router.include(ProposalRecord, 'proposals/<int:proposal_id>')
 
+        # Vehicles
         Router.include(VehicleNew, 'vehicles')
         Router.include(VehicleRecord, 'vehicles/<int:vehicle_id>')
