a213e87d1e66cca751d01b2b0d190758a26b4b0f
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-05-07 18:56:15 -0300

    feat: implementa esboço de schemas de representação

--- src/mapi/schemas.py
+++ src/mapi/schemas.py
@@ -5,13 +5,48 @@
 """
 
 from mapi.orm import Schema
-from mapi.models import Worker
+from mapi.models import (Address, Company, Entity, Item,
+                         Offer, Proposal, Vehicle, Worker)
+
+
+class AddressSchema(Schema):
+    """Schema de representação de dados da entidade Address."""
+    class Meta:
+        model = Address
+
+
+class CompanySchema(Schema):
+    """Schema de representação de dados da entidade Company."""
+    class Meta:
+        model = Company
+
+
+class ItemSchema(Schema):
+    """Schema de representação de dados da entidade Item."""
+    class Meta:
+        model = Item
+
+
+class OfferSchema(Schema):
+    """Schema de representação de dados da entidade Offer."""
+    class Meta:
+        model = Offer
+
+
+class ProposalSchema(Schema):
+    """Schema de representação de dados da entidade Proposal."""
+    class Meta:
+        model = Proposal
+
+
+class VehicleSchema(Schema):
+    """Schema de representação de dados da entidade Vehicle."""
+    class Meta:
+        model = Vehicle
 
 
 class WorkerSchema(Schema):
-    """
-    --- TODO: DOCUMENTATION ---
-    """
+    """Schema de representação de dados da entidade Worker."""
     class Meta:
         fields = ('name', 'email', 'telephone', 'rg', 'cpf', 'license_type',
                   'address_assoc')
