3effab2c8eb1774225a578343bff9ded42bed54f
Caian R. Ertl <caianrais@protonmail.com>
2018-05-07 00:46:54 -0300

    feat: importa módulo de schemas no inicializador dos recursos

--- src/mapi/resources/__init__.py
+++ src/mapi/resources/__init__.py
@@ -13,5 +13,8 @@ sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 
 from mapi import (db, request, Resource, Formatter)
+
 from mapi.models import *
+from mapi.schemas import *
+
 from mapi.resources.responder import response
