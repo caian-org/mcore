2155fdfa60a82fb2698391371409d42bd29484a0
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-05-07 19:13:40 -0300

    feat: implementa estrutura básica para o CRUD do recurso Company

---
+++ src/mapi/resources/company.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+"""
+--- TODO: DOCUMENTATION ---
+"""
+
+# Database connection
+from . import db
+
+# Worker model
+from . import Company
+
+# HTTP-related
+from . import (Resource, request, response)
+
+# Form/JSON data authenticator
+from .auth import Authenticator
+
+
+class CompanyRecord(Resource):
+    """
+    --- TODO: DOCUMENTATION ---
+    """
+
+    def delete(self):
+        """
+        --- TODO: DOCUMENTATION ---
+        """
+        pass
+
+    def get(self):
+        """
+        --- TODO: DOCUMENTATION ---
+        """
+        pass
+
+    def post(self):
+        """
+        --- TODO: DOCUMENTATION ---
+        """
+        pass
+
+    def put(self):
+        """
+        --- TODO: DOCUMENTATION ---
+        """
+        pass
