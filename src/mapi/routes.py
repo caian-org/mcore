7154131cc4bd9b9495ab292865cfe8460ac48540
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-05-07 20:14:23 -0300

    feat: inclui rotas de inclusão para todos os recursos

--- src/mapi/routes.py
+++ src/mapi/routes.py
@@ -5,16 +5,16 @@
 """
 
 
-from mapi import rapi
-from mapi import Formatter
+from mapi import (rapi, Formatter)
 
-from mapi.resources.address import (AddressRecord, AddressNew)
-from mapi.resources.worker import (WorkerRecord, WorkerAuth)
-from mapi.resources.company import (CompanyRecord)
-from mapi.resources.item import (ItemRecord)
-from mapi.resources.offer import (OfferRecord)
-from mapi.resources.proposal import (ProposalRecord)
-from mapi.resources.vehicle import (VehicleRecord)
+from mapi.resources.worker   import (WorkerRecord, WorkerNew, WorkerAuth)
+from mapi.resources.company  import (CompanyRecord, CompanyNew)
+
+from mapi.resources.address  import (AddressRecord, AddressNew)
+from mapi.resources.item     import (ItemRecord, ItemNew)
+from mapi.resources.offer    import (OfferRecord, OfferNew)
+from mapi.resources.proposal import (ProposalRecord, ProposalNew)
+from mapi.resources.vehicle  import (VehicleRecord, VehicleNew)
 
 
 class Router:
@@ -35,12 +35,24 @@ class Router:
         --- TODO: DOCUMENTATION ---
         """
 
+        Router.include(WorkerNew, 'workers')
         Router.include(WorkerAuth, 'workers/auth')
         Router.include(WorkerRecord, 'workers/<int:worker_id>')
+
         Router.include(AddressNew, 'addresses')
         Router.include(AddressRecord, 'addresses/<int:address_id>')
+
+        Router.include(CompanyNew, 'companies')
         Router.include(CompanyRecord, 'companies/<int:company_id>')
+
+        Router.include(ItemNew, 'items')
         Router.include(ItemRecord, 'items/<int:item_id>')
+
+        Router.include(OfferNew, 'offers')
         Router.include(OfferRecord, 'offers/<int:offers_id>')
+
+        Router.include(ProposalNew, 'proposals')
         Router.include(ProposalRecord, 'proposals/<int:proposal_id>')
+
+        Router.include(VehicleNew, 'vehicles')
         Router.include(VehicleRecord, 'vehicles/<int:vehicle_id>')
