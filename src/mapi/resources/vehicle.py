593020358b268e02c012fb99ec3a098681817ddc
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-05-07 19:15:06 -0300

    feat: implementa estrutura básica para o CRUD do recurso Vehicle

---
+++ src/mapi/resources/vehicle.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+"""
+--- TODO: DOCUMENTATION ---
+"""
+
+# Database connection
+from . import db
+
+# Worker model
+from . import Vehicle
+
+# HTTP-related
+from . import (Resource, request, response)
+
+# Form/JSON data authenticator
+from .auth import Authenticator
+
+
+class VehicleRecord(Resource):
+    """
+    --- TODO: DOCUMENTATION ---
+    """
+
+    def delete(self):
+        """
+        --- TODO: DOCUMENTATION ---
+        """
+        pass
+
+    def get(self):
+        """
+        --- TODO: DOCUMENTATION ---
+        """
+        pass
+
+    def post(self):
+        """
+        --- TODO: DOCUMENTATION ---
+        """
+        pass
+
+    def put(self):
+        """
+        --- TODO: DOCUMENTATION ---
+        """
+        pass
