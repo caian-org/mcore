3477425e2e623b656310b56877a32105ff9a40d8
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-05-07 19:37:09 -0300

    feat: simplificação de código, adição das rotas de CRUD

--- src/mapi/routes.py
+++ src/mapi/routes.py
@@ -10,20 +10,37 @@ from mapi import Formatter
 
 from mapi.resources.address import (AddressRecord, AddressNew)
 from mapi.resources.worker import (WorkerRecord, WorkerAuth)
+from mapi.resources.company import (CompanyRecord)
+from mapi.resources.item import (ItemRecord)
+from mapi.resources.offer import (OfferRecord)
+from mapi.resources.proposal import (ProposalRecord)
+from mapi.resources.vehicle import (VehicleProposal)
 
 
 class Router:
     """
     --- TODO: DOCUMENTATION ---
     """
+    @staticmethod
+    def include(resource, route):
+        """
+        --- TODO: DOCUMENTATION ---
+        """
+
+        rapi.add_resource(resource, Formatter.gen_route(route))
 
     @staticmethod
     def act():
         """
         --- TODO: DOCUMENTATION ---
         """
-        rapi.add_resource(WorkerAuth, Formatter.gen_route('workers/auth'))
-        rapi.add_resource(WorkerRecord, Formatter.gen_route('workers/<int:worker_id>'))
 
-        rapi.add_resource(AddressNew, Formatter.gen_route('addresses'))
-        rapi.add_resource(AddressRecord, Formatter.gen_route('addresses/<int:address_id>'))
+        Router.include(WorkerAuth, 'workers/auth')
+        Router.include(WorkerRecord, 'workers/<int:worker_id>')
+        Router.include(AddressNew, 'addresses')
+        Router.include(AddressRecord, 'addresses/<int:address_id>')
+        Router.include(CompanyRecord, 'companies/<int:company_id>')
+        Router.include(ItemRecord, 'items/<int:item_id>')
+        Router.include(OfferRecord, 'offers/<int:offers_id>')
+        Router.include(ProposalRecord, 'proposals/<int:proposal_id>')
+        Router.include(VehicleProposal, 'vehicles/<int:vehicle_id>')
