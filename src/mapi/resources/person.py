95f6d47a9d2b9ed5f62ede4fea17969af89d80fd
Caian R. Ertl <caian.ertl@rivendel.com.br>
2018-05-09 19:38:00 -0300

    feat: implementa método GET no CRUD

--- src/mapi/resources/person.py
+++ src/mapi/resources/person.py
@@ -7,8 +7,14 @@
 # Worker model
 from . import Person
 
+# Person datatype schema
+from . import PersonSchema
+
 # HTTP-related
 from . import (Resource, request, response)
+
+# Form/JSON data authenticator
+from .auth import Authenticator
 
 
 class PersonAuth(Resource):
@@ -43,3 +49,46 @@ class PersonAuth(Resource):
         data = {}
         data['token'] = token.decode('ascii')
         return response.SUCCESS(200, data)
+
+
+class PersonRecord(Resource):
+    """
+    --- TODO: DOCUMENTATION ---
+    """
+    entity = Person
+    schema = PersonSchema
+
+    def delete(self):
+        """
+        --- TODO: DOCUMENTATION ---
+        """
+        pass
+
+    def get(self, uid):
+        """
+        --- TODO: DOCUMENTATION ---
+        """
+        payload = request.get_json()
+
+        if not Authenticator.check_struct(payload, ['auth']):
+            return response.bad_request
+
+        auth  = payload['auth']
+        token = auth.get('token')
+
+        if not token:
+            return response.bad_request
+
+        if not Authenticator.verify_token(token):
+            return response.forbidden
+
+        person = self.entity.query.get(uid)
+
+        data = self.schema().dump(person).data
+        return response.SUCCESS(200, data)
+
+    def put(self):
+        """
+        --- TODO: DOCUMENTATION ---
+        """
+        pass
