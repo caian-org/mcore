7583386356981cc2d092b7c6c2cdf6a9335d233e
Caian R. Ertl <caianrais@protonmail.com>
2018-05-12 18:23:28 -0300

    refact: integra módulo secret em config

--- src/config.py
+++ src/config.py
@@ -10,6 +10,10 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
+from mapi import Faker
+_fake = Faker()
+
+
 class Config:
     PORT = 80
     DEBUG = False
@@ -17,7 +21,11 @@ class Config:
     SQLALCHEMY_DATABASE_URI = ''
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    SECRET_KEY = None
+    SECRET_KEY = _fake.password(length=128,
+                                special_chars=True,
+                                digits=True,
+                                upper_case=True,
+                                lower_case=True)
 
 
 class Production(Config):
@@ -28,3 +36,8 @@ class Development(Config):
     PORT = 8080
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+
+
+config = Production
+if os.environ.get('TEST_ENVIRON'):
+    config = Development
