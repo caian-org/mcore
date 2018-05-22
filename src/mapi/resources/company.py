0aacc29a91a08d663614ef3b34449d03b70212bc
Caian R. Ertl <caianrais@protonmail.com>
2018-05-22 03:48:51 -0300

    feat: implementa método de cadastro de empresa

--- src/mapi/resources/company.py
+++ src/mapi/resources/company.py
@@ -1,70 +1,101 @@
 # -*- coding: utf-8 -*-
 
-"""
---- TODO: DOCUMENTATION ---
-"""
+# ...
+from datetime import datetime
 
 # Database connection
 from . import db
 
 # Worker model
 from . import Company
+from . import Address
+from . import CompanyHasAddresses
 
 # Worker datatype schema
 from . import CompanySchema
 
 # HTTP-related
-from . import (Resource, request, response)
+from . import Resource
+from . import request
+from . import response
+
+# Authentication logic
+from .person import PersonNew
+from .person import PersonAuth
+from .person import PersonRecord
 
 # Form/JSON data authenticator
 from .auth import Authenticator
 
-# Authentication logic
-from .person import (PersonAuth, PersonRecord)
-
 
 class CompanyAuth(PersonAuth):
-    """
-    --- TODO: DOCUMENTATION ---
-    """
-
     entity = Company
 
 
 class CompanyNew(Resource):
-    """
-    --- TODO: DOCUMENTATION ---
-    """
-
     def post(self):
-        """
-        --- TODO: DOCUMENTATION ---
-        """
-        pass
+        payload = request.get_json()
+
+        if not Authenticator.check_struct(payload, ['data']):
+            return response.bad_request
+
+        data = payload['data']
+        required = ['address']
+        if not Authenticator.check_struct(data, required):
+            return response.bad_request
+
+        address = data['address']
+
+        params = [
+            data.get('name'),
+            data.get('email'),
+            data.get('cnpj'),
+            data.get('opening'),
+            data.get('telephone'),
+            data.get('password'),
+            address.get('number'),
+            address.get('postcode')
+        ]
+
+        if not Authenticator.check_payload(params):
+            return response.bad_request
+
+        data['opening'] = datetime.strptime(data['opening'], '%d-%m-%Y')
+
+        company = self.new(name=data['name'],
+                           email=data['email'],
+                           cnpj=data['cnpj'],
+                           opening=data['opening'],
+                           telephone=data['telephone'])
+
+        company.set_password(data['password'])
+
+        address = Address(postcode=address['postcode'],
+                          number=address['number'],
+                          complement=address['complement'])
+
+        db.session.add(company)
+        db.session.add(address)
+        db.session.commit()
+
+        company_address = CompanyHasAddresses(company=company, address=address)
+
+        db.session.add(company_address)
+        db.session.commit()
+
+        return response.SUCCESS(201, 'ok')
 
 
 class CompanyRecord(PersonRecord):
-    """
-    --- TODO: DOCUMENTATION ---
-    """
     entity = Company
     schema = CompanySchema
 
     def delete(self):
-        """
-        --- TODO: DOCUMENTATION ---
-        """
         pass
 
     def put(self):
-        """
-        --- TODO: DOCUMENTATION ---
-        """
         pass
 
 
 class CompanyProposals(Resource):
-    """
-    --- TODO: DOCUMENTATION ---
-    """
     pass
