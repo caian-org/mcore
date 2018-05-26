4fc30b1a562b1a2c8585fef2ffa72f323ae52e23
Caian R. Ertl <caianrais@protonmail.com>
2018-05-26 20:37:40 -0300

    feat: implementa lógica de cadastro de administradores

--- src/mapi/resources/admin.py
+++ src/mapi/resources/admin.py
@@ -1,45 +1,99 @@
 # -*- coding: utf-8 -*-
 
-"""
---- TODO: DOCUMENTATION ---
-"""
+# ...
+from datetime import datetime
 
 # Database connection
 from . import db
 
-# Admin model
+# Worker model and related ones
 from . import Admin
+from . import Address
+from . import AdminHasAddresses
 
 # Admin datatype schema
 from . import AdminSchema
 
 # HTTP-related
-from . import (Resource, request, response)
+from . import Resource
+from . import request
+from . import response
 
 # Authentication logic
-from .person import (PersonAuth, PersonRecord, PersonNew)
+from .person import PersonNew
+from .person import PersonAuth
+from .person import PersonRecord
+
+# Form/JSON data authenticator
+from .auth import Authenticator
 
 
 class AdminAuth(PersonAuth):
-    """
-    --- TODO: DOCUMENTATION ---
-    """
-
     entity = Admin
 
 
 class AdminNew(PersonNew):
-    """
-    --- TODO: DOCUMENTATION ---
-    """
-
     entity = Admin
     schema = AdminSchema
 
+    def post(self):
+        payload = request.get_json()
+
+        if not Authenticator.check_struct(payload, ['data']):
+            return response.bad_request
+
+        data = payload['data']
+        required = ['address']
+        if not Authenticator.check_struct(data, required):
+            return response.bad_request
+
+        address = data['address']
+
+        params = [
+            data.get('name'),
+            data.get('telephone'),
+            data.get('email'),
+            data.get('password'),
+            data.get('rg'),
+            data.get('cpf'),
+            data.get('gender'),
+            data.get('birthday'),
+            data.get('authorityLevel'),
+            address.get('number'),
+            address.get('postcode')
+        ]
+
+        if not Authenticator.check_payload(params):
+            return response.bad_request
+
+        data['birthday'] = datetime.strptime(data['birthday'], '%d-%m-%Y')
+
+        admin = self.new(name=data['name'],
+                         telephone=data['telephone'],
+                         email=data['email'],
+                         rg=data['rg'],
+                         cpf=data['cpf'],
+                         gender=data['gender'],
+                         birthday=data['birthday'],
+                         authority_level=data['authorityLevel'])
+
+        admin.set_password(data['password'])
+
+        address = Address(postcode=address['postcode'],
+                          number=address['number'],
+                          complement=address['complement'])
+
+        db.session.add(admin)
+        db.session.add(address)
+        db.session.commit()
+
+        admin_address = AdminHasAddresses(admin=admin, address=address)
+
+        db.session.add(admin_address)
+        db.session.commit()
+
+        return response.created('admins', admin.uid)
 
 class AdminRecord(PersonRecord):
-    """
-    --- TODO: DOCUMENTATION ---
-    """
     entity = Admin
     schema = AdminSchema
