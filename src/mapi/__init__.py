af0efa4e47cfa0add87c824f3cae80de407e2f59
Caian R. Ertl <caianrais@protonmail.com>
2018-05-31 22:08:13 -0300

    refact: realiza importação linha-a-linha

--- src/mapi/__init__.py
+++ src/mapi/__init__.py
@@ -5,7 +5,8 @@ __version__ = 'v1'
 
 
 # Utils
-from mapi.utils import (Exit, Formatter)
+from mapi.utils import Exit
+from mapi.utils import Formatter
 
 
 #   ____       _                    _
@@ -21,25 +22,29 @@ try:
     from flask import Flask, request
 
     # REST API abstraction layer for Flask
-    from flask_restful import (Api as Restful, Resource)
+    from flask_restful import Resource
+    from flask_restful import Api as Restful
 
     # ORM-related
     from flask_migrate import Migrate
+    from sqlalchemy.orm import joinedload
     from flask_sqlalchemy import SQLAlchemy
     from flask_marshmallow import Marshmallow
 
     # Security modules (login-related)
-    from werkzeug.security import (generate_password_hash as gen_phash,
-                                   check_password_hash as check_phash)
+    from werkzeug.security import generate_password_hash as gen_phash
+    from werkzeug.security import check_password_hash as check_phash
 
     # Security modules (token-related)
-    from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer,
-                              BadSignature, SignatureExpired)
+    from itsdangerous import BadSignature
+    from itsdangerous import SignatureExpired
+    from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
     # Correios API
     import pycep_correios as cep_api
-    from pycep_correios.excecoes import (Timeout, FalhaNaConexao,
-                                         MultiploRedirecionamento)
+    from pycep_correios.excecoes import Timeout
+    from pycep_correios.excecoes import FalhaNaConexao
+    from pycep_correios.excecoes import MultiploRedirecionamento
 
 except ImportError as error:
     Exit.with_fail('Impossible to import 3rd-party libraries\n'
