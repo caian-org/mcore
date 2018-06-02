65c08165f47f03240af6123a6afc52168e0b1496
Caian R. Ertl <caianrais@protonmail.com>
2018-06-01 23:37:24 -0300

    feat: implementa listagem de propostas abertas, [...]
    * Inclui documentação dos métodos
    * Inclui novos atributos no cadastro de propostas
    * Inclui novas bibliotecas

--- src/mapi/resources/proposal.py
+++ src/mapi/resources/proposal.py
@@ -3,8 +3,12 @@
 # ...
 from datetime import datetime
 
+# ...
+from . import CEP
+
 # Database connection
 from . import db
+from . import joinedload
 
 # Proposal model
 from . import Proposal
@@ -14,6 +18,7 @@ from . import Item
 
 # Proposal datatype schema
 from . import ProposalSchema
+from . import ProposalsListSchema
 
 # HTTP-related
 from . import Resource
@@ -24,8 +29,29 @@ from . import response
 from .auth import Authenticator
 
 
-class ProposalNew(Resource):
+class ProposalResource(Resource):
+    def get(self):
+        '''
+        Implementação do método GET em /proposals/
+
+        Este método é capaz de retornar todas as propostas criadas com status
+        "em aberto". Requer autenticação e só pode ser consumido através de uma
+        conta corporativa (companu account).
+        '''
+        opened_proposals = Proposal.query.filter_by(status=True).all()
+        proposal_schema = ProposalsListSchema(many=True)
+
+        data = proposal_schema.dump(opened_proposals)
+        return response.ok(data[0])
+
     def post(self):
+        '''
+        Implementação do método POST em /proposals/
+
+        Este método é capaz de cadastrar uma proposta mediante indicação dos
+        dados necessários. Requer autenticação e só pode ser consumido através
+        de uma conta corporativa (company account).
+        '''
         payload = request.get_json()
 
         # Verifica a estrutura da payload recebida
@@ -48,6 +74,7 @@ class ProposalNew(Resource):
         origin_addr = data['originAddress']
         destin_addr = data['destinationAddress']
         params = [
+            data.get('title'),
             data.get('status'),
             data.get('deadline'),
             data.get('companyUid'),
@@ -58,6 +85,11 @@ class ProposalNew(Resource):
         if not Authenticator.check_payload(params):
             return response.bad_request
 
+        # Verifica se o id recebido é de uma empresa existente
+        company = Company.query.get(data['companyUid'])
+        if not company:
+            return response.bad_request
+
         # Verifica se existe ao menos um item na lista
         items = data['items']
         if not items:
@@ -66,37 +98,35 @@ class ProposalNew(Resource):
         # Verifica em cada um dos itens se os atributos necessários constam
         for i in items:
             required_attrs = [
-                i.get('fragile'),
-                i.get('weight'),
+                i.get('title'),
                 i.get('width'),
-                i.get('height')]
+                i.get('height'),
+                i.get('weight'),
+                i.get('fragile')]
             if not Authenticator.check_payload(required_attrs):
                 return response.bad_request
 
         # Converte o formato de data 'dd-mm-aaaa' em uma datetime
         data['deadline'] = datetime.strptime(data['deadline'], '%d-%m-%Y')
 
-        # Verifica se o id recebido é de uma empresa existente
-        company = Company.query.get(data['companyUid'])
-        if not company:
-            return response.bad_request
-
         # Insere no banco os endereços de origem e destino
         origin = Address(postcode=origin_addr['postcode'],
                          number=origin_addr['number'],
-                         complement=origin_addr['complement'])
+                         complement=origin_addr.get('complement'))
 
         destin = Address(postcode=destin_addr['postcode'],
                          number=destin_addr['number'],
-                         complement=destin_addr['complement'])
+                         complement=destin_addr.get('complement'))
 
         db.session.add(origin)
         db.session.add(destin)
         db.session.commit()
 
         # Insere no banco a proposta
-        proposal = Proposal(status=data['status'],
+        proposal = Proposal(title=data['title'],
+                            status=data['status'],
                             deadline=data['deadline'],
+                            description=data.get('description'),
                             origin=origin,
                             destination=destin,
                             company=company)
@@ -105,10 +135,11 @@ class ProposalNew(Resource):
 
         # Insere no banco todos os itens, referenciando a proposta
         for i in items:
-            item = Item(fragile=i['fragile'],
-                        weight=i['weight'],
+            item = Item(title=i['title'],
                         width=i['width'],
                         height=i['height'],
+                        weight=i['weight'],
+                        fragile=i['fragile'],
                         proposal=proposal)
 
             db.session.add(item)
@@ -126,7 +157,3 @@ class ProposalRecord(Resource):
 
     def put(self):
         pass
-
-
-class ProposalOffers(Resource):
-    pass
