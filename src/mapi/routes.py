34a23314126764c4680b2e70856f5b049395f989
Caian R. Ertl <caianrais@protonmail.com>
2018-06-05 02:52:43 -0300

    fix: organiza imports, remove rotas desnecessárias/redundantes

--- src/mapi/routes.py
+++ src/mapi/routes.py
@@ -3,11 +3,17 @@
 from mapi import rapi
 from mapi import Formatter
 
-from mapi.resources.admin    import *
-from mapi.resources.worker   import *
-from mapi.resources.company  import *
-from mapi.resources.offer    import *
-from mapi.resources.proposal import *
+from mapi.resources.admin import AdminNew
+from mapi.resources.admin import AdminAuth
+from mapi.resources.worker import WorkerNew
+from mapi.resources.worker import WorkerAuth
+from mapi.resources.worker import WorkerRecord
+from mapi.resources.company import CompanyNew
+from mapi.resources.company import CompanyAuth
+from mapi.resources.company import CompanyRecord
+from mapi.resources.proposal import ProposalOffer
+from mapi.resources.proposal import ProposalRecord
+from mapi.resources.proposal import ProposalResource
 
 
 class Router:
@@ -26,19 +32,13 @@ class Router:
         Router.include(WorkerNew, 'workers')
         Router.include(WorkerAuth, 'workers/auth')
         Router.include(WorkerRecord, 'workers/<int:uid>')
-        Router.include(WorkerAddresses, 'workers/<int:uid>/addresses')
-        Router.include(WorkerVehicles, 'workers/<int:uid>/vehicles')
 
         # Companies
         Router.include(CompanyNew, 'companies')
         Router.include(CompanyAuth, 'companies/auth')
         Router.include(CompanyRecord, 'companies/<int:uid>')
-        Router.include(CompanyProposals, 'companies/<int:uid>/proposals')
-
-        # Offers
-        Router.include(OfferNew, 'offers')
-        Router.include(OfferRecord, 'offers/<int:uid>')
 
         # Proposals
         Router.include(ProposalResource, 'proposals')
         Router.include(ProposalRecord, 'proposals/<int:uid>')
+        Router.include(ProposalOffer, 'proposals/<int:uid>/offer')
